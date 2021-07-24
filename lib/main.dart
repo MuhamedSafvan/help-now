99f58eada9fc68ef4d9a3b1be4f1db72b7e27436
MuhamedSafvan <safumanoor66@gmail.com>
2021-07-24 19:50:50 +0530

    Modified main file

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,23 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text("Help Now"),
         ),
-      ),
-    );
+      );
   }
 }
